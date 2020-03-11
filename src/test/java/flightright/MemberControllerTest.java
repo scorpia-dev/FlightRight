657a58a984d1cd10a3c7bf64ec0aa22689d1457b
nick prendergast <nick_prendergast@hotmail.com>
2020-03-11 18:41:58 +0000

    rewrote unit test

--- src/test/java/flightright/MemberControllerTest.java
+++ src/test/java/flightright/MemberControllerTest.java
@@ -13,11 +13,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,15 +59,49 @@ public class MemberControllerTest {
 	ObjectMapper objectMapper;
 
 	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
+	Member member;
+	Member member2;
+	Member member3;
+	File file;
+	Date dob;
+	Date dob2;
+	Date dob3;
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		String sDate = "1993-09-21";
+		String sDate2 = "1963-11-02";
+		String sDate3 = "1995-01-27";
+
+		Date dob = formatter.parse(sDate);
+		Date dob2 = formatter.parse(sDate2);
+		Date dob3 = formatter.parse(sDate3);
+
+		File file = File.createTempFile("image", ".jpg");
+		File file2 = File.createTempFile("test2", ".png");
+		File file3 = File.createTempFile("test3", ".jpeg");
+
+		member = new Member("Nick", "Prendergast", dob, "NR14 7TP", file);
+		member2 = new Member("John", "Cena", dob2, "WA3 8BJ", file2);
+		member3 = new Member("Jenny", "Mann", dob3, "IP1 9XJ", file3);
+
+		/*
+		 * drivingLog=new DrivingLog(); duration1 = new Duration(1, 30, 45); duration2 =
+		 * new Duration(2, 50, 12);
+		 * 
+		 * drivingRecord1 = new DrivingRecord(230.0, duration1, "This was a long trip");
+		 * drivingRecord2 = new DrivingRecord(300.0, duration2,
+		 * "This trip is even longer.");
+		 * 
+		 * drivingLog.addDrivingRecord(drivingRecord1);
+		 * drivingLog.addDrivingRecord(drivingRecord2);
+		 */
+
+	}
 
 	@Transactional
 	@Test
 	public void createMemberTest() throws Exception {
-		String sDate = "1993-09-21";
-		Date dob = formatter.parse(sDate);
-		File file = File.createTempFile("image", ".jpg");
-
-		Member member = new Member("Nick", "Prendergast", dob, "NR14 7TP", file);
 
 		String json = objectMapper.writeValueAsString(member);
 
@@ -74,8 +112,8 @@ public class MemberControllerTest {
 				.andExpect(MockMvcResultMatchers.jsonPath("firstName").value("Nick"))
 				.andExpect(MockMvcResultMatchers.jsonPath("lastName").value("Prendergast"))
 				.andExpect(MockMvcResultMatchers.jsonPath("dateOfBirth").value("1993-09-21"))
-				.andExpect(MockMvcResultMatchers.jsonPath("postalCode").value("NR14 7TP"))
-				.andExpect(MockMvcResultMatchers.jsonPath("picture").value(file.toString()));
+				.andExpect(MockMvcResultMatchers.jsonPath("postalCode").value("NR14 7TP")).andExpect(
+						MockMvcResultMatchers.jsonPath("picture").value(org.hamcrest.Matchers.containsString(".jpg")));
 
 		Member newMember = memberService.getMember(1L);
 		assertThat(newMember.getId().equals(1L));
@@ -90,11 +128,7 @@ public class MemberControllerTest {
 	@Transactional
 	@Test
 	public void deleteMemberTest() throws Exception {
-		String sDate = "1957-12-25";
-		Date dob = formatter.parse(sDate);
-		File file = File.createTempFile("image", ".jpg");
 
-		Member member = new Member("James", "Roberts", dob, "NR14 7TP", file);
 		memberService.createMember(member);
 
 		mvc.perform(delete("/members/{id}", 1).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
@@ -109,20 +143,15 @@ public class MemberControllerTest {
 	@Transactional
 	@Test
 	public void getMemberTest() throws Exception {
-		String sDate = "1973-09-21";
-		Date dob = formatter.parse(sDate);
-		File file = File.createTempFile("image", ".jpg");
-
-		Member member = new Member("Timmy", "Jenkins", dob, "SA14 9AS", file);
 		memberService.createMember(member);
 
 		mvc.perform(get("/members/{id}", 1).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
 				.andDo(print()).andExpect(MockMvcResultMatchers.jsonPath("id").value(1))
-				.andExpect(MockMvcResultMatchers.jsonPath("firstName").value("Timmy"))
-				.andExpect(MockMvcResultMatchers.jsonPath("lastName").value("Jenkins"))
-				.andExpect(MockMvcResultMatchers.jsonPath("dateOfBirth").value("1973-09-21"))
-				.andExpect(MockMvcResultMatchers.jsonPath("postalCode").value("SA14 9AS"))
-				.andExpect(MockMvcResultMatchers.jsonPath("picture").value(file.toString()));
+				.andExpect(MockMvcResultMatchers.jsonPath("firstName").value("Nick"))
+				.andExpect(MockMvcResultMatchers.jsonPath("lastName").value("Prendergast"))
+				.andExpect(MockMvcResultMatchers.jsonPath("dateOfBirth").value("1993-09-21"))
+				.andExpect(MockMvcResultMatchers.jsonPath("postalCode").value("NR14 7TP")).andExpect(
+						MockMvcResultMatchers.jsonPath("picture").value(org.hamcrest.Matchers.containsString(".jpg")));
 
 		assertTrue(memberService.getMember(1L) == member);
 	}
@@ -130,47 +159,34 @@ public class MemberControllerTest {
 	@Transactional
 	@Test
 	public void listExsistingMembersTest() throws Exception {
-		String sDate1 = "1973-09-21";
-		String sDate2 = "1963-11-02";
-		String sDate3 = "1995-01-27";
-
-		Date dob1 = formatter.parse(sDate1);
-		Date dob2 = formatter.parse(sDate2);
-		Date dob3 = formatter.parse(sDate3);
-
-		File file1 = File.createTempFile("test1", ".jpg");
-		File file2 = File.createTempFile("test2", ".jpg");
-		File file3 = File.createTempFile("test3", ".jpg");
-
-		Member member1 = new Member("Timmy", "Jenkins", dob1, "SA14 9AS", file1);
-		Member member2 = new Member("John", "Cena", dob2, "WA3 8BJ", file2);
-		Member member3 = new Member("Jenny", "Mann", dob3, "IP1 9XJ", file3);
-
-		memberService.createMember(member1);
+		memberService.createMember(member);
 		memberService.createMember(member2);
 		memberService.createMember(member3);
 
 		mvc.perform(get("/members").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andDo(print())
 				.andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0].firstName").value("Timmy"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0].lastName").value("Jenkins"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0].dateOfBirth").value("1973-09-21"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0].postalCode").value("SA14 9AS"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[0].picture").value(file1.toString()))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].firstName").value("Nick"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].lastName").value("Prendergast"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].dateOfBirth").value("1993-09-21"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].postalCode").value("NR14 7TP"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[0].picture")
+						.value(org.hamcrest.Matchers.containsString(".jpg")))
 
 				.andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(2))
 				.andExpect(MockMvcResultMatchers.jsonPath("$[1].firstName").value("John"))
 				.andExpect(MockMvcResultMatchers.jsonPath("$[1].lastName").value("Cena"))
 				.andExpect(MockMvcResultMatchers.jsonPath("$[1].dateOfBirth").value("1963-11-02"))
 				.andExpect(MockMvcResultMatchers.jsonPath("$[1].postalCode").value("WA3 8BJ"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[1].picture").value(file2.toString()))
+				.andExpect(MockMvcResultMatchers.jsonPath("$[1].picture")
+						.value(org.hamcrest.Matchers.containsString(".png")))
 
 				.andExpect(MockMvcResultMatchers.jsonPath("$[2].id").value(3))
 				.andExpect(MockMvcResultMatchers.jsonPath("$[2].firstName").value("Jenny"))
 				.andExpect(MockMvcResultMatchers.jsonPath("$[2].lastName").value("Mann"))
 				.andExpect(MockMvcResultMatchers.jsonPath("$[2].dateOfBirth").value("1995-01-27"))
 				.andExpect(MockMvcResultMatchers.jsonPath("$[2].postalCode").value("IP1 9XJ"))
-				.andExpect(MockMvcResultMatchers.jsonPath("$[2].picture").value(file3.toString()));
+				.andExpect(MockMvcResultMatchers.jsonPath("$[2].picture")
+						.value(org.hamcrest.Matchers.containsString(".jpeg")));
 
 		assertTrue(memberService.getAllMembers().size() == 3);
 	}
@@ -178,10 +194,7 @@ public class MemberControllerTest {
 	@Transactional
 	@Test
 	public void partialUpdateMemberTest() throws Exception {
-		String sDate = "1993-09-21";
-		Date dob = formatter.parse(sDate);
-		File file = File.createTempFile("image", ".jpg");
-		Member member = new Member("Nick", "Prendergast", dob, "NR14 7TP", file);
+
 		memberService.createMember(member);
 
 		Long id = member.getId();
@@ -198,8 +211,9 @@ public class MemberControllerTest {
 				.andExpect(MockMvcResultMatchers.jsonPath("lastName").value("Prendergast"))
 				.andExpect(MockMvcResultMatchers.jsonPath("dateOfBirth").value("1993-09-21"))
 				.andExpect(MockMvcResultMatchers.jsonPath("postalCode").value("NR14 7TP"))
-				.andExpect(MockMvcResultMatchers.jsonPath("picture").value(file.toString()));
-
+				.andExpect(MockMvcResultMatchers.jsonPath("picture")
+						.value(org.hamcrest.Matchers.containsString(".jpg")));
+		
 		Member updatedMember = memberService.getMember(1L);
 		assertThat(updatedMember.getId().equals(1L));
 		assertThat(updatedMember.getFirstName().equals("Tom"));
@@ -213,14 +227,9 @@ public class MemberControllerTest {
 	@Transactional
 	@Test
 	public void updateMemberAllFieldsTest() throws Exception {
-		String sDate1 = "1993-09-21";
-		Date dob1 = formatter.parse(sDate1);
-		String sDate2 = "1990-02-03";
-		Date dob2 = formatter.parse(sDate2);
-		File file1 = File.createTempFile("image", ".jpg");
-		File file2 = File.createTempFile("image", ".jpg");
 
-		Member member = new Member("Nick", "Prendergast", dob1, "NR14 7TP", file1);
+		File file2 = File.createTempFile("image", ".png");
+
 		memberService.createMember(member);
 
 		Long id = member.getId();
@@ -238,9 +247,9 @@ public class MemberControllerTest {
 				.andExpect(MockMvcResultMatchers.jsonPath("id").value(1))
 				.andExpect(MockMvcResultMatchers.jsonPath("firstName").value("Tom"))
 				.andExpect(MockMvcResultMatchers.jsonPath("lastName").value("Jones"))
-				.andExpect(MockMvcResultMatchers.jsonPath("dateOfBirth").value("1990-02-03"))
+				.andExpect(MockMvcResultMatchers.jsonPath("dateOfBirth").value("1963-11-02"))
 				.andExpect(MockMvcResultMatchers.jsonPath("postalCode").value("NR1 1BD"))
-				.andExpect(MockMvcResultMatchers.jsonPath("picture").value(file2.toString()));
+				.andExpect(MockMvcResultMatchers.jsonPath("picture").value(org.hamcrest.Matchers.containsString(".png")));
 
 		Member afterSaveUpdatedMember = memberService.getMember(1L);
 		assertThat(afterSaveUpdatedMember.getId().equals(id));
